58b9901b0242064336297b6445825d5e475652da
Ever Rojas <Everjosejr@hotmail.com>
2022-06-05 19:49:00 -0500

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-
-import { createRoot } from 'react-dom/client';
-import App from './routes/App'
-const container = document.querySelector('#app');
-const root = createRoot(container);
-root.render(<App/>)
