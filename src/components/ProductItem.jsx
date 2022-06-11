ab27111141e8941ba55cfb3263b79f5d1bf3da89
Ever Rojas <Everjosejr@hotmail.com>
2022-06-10 20:30:31 -0500

    ⚡️ improve perfomance

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,13 +2,17 @@ import React, {useContext} from 'react';
 import '@styles/ProductItem.scss';
 import AppContext from '@context/AppContext'; 
 import addToCartImg from '@icons/bt_add_to_cart.svg';
+import addedToCartImg from "@icons/bt_added_to_cart.svg"
 
 const ProductItem = ({product}) => {
-	const { addToCart } = useContext(AppContext);
-
+	const { addToCart, removeFromCart, state } = useContext(AppContext);
+	
 	const handleClick = item => {
-		addToCart(item);
+		itsProductAdded() ? removeFromCart(item) : addToCart(item)
 	}
+	const itsProductAdded = () => 
+	state.cart.some((item) => item.id === product.id) ? true : false;
+
 	return (
 		<div className="ProductItem">
 			<img src={product.images[0]} alt={product.title} />
@@ -18,7 +22,11 @@ const ProductItem = ({product}) => {
 					<p>{product.title}</p>
 				</div>
 				<figure onClick={() => handleClick(product)} >
-					<img src={addToCartImg} alt="" />
+					{ itsProductAdded() ? (
+					<img src={addedToCartImg} alt="Cart" />
+					) : (
+					<img src={addToCartImg} alt="Cart" />
+					)}
 				</figure>
 			</div>
 		</div>
